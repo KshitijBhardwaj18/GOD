8ae51809bdbc5a18774952ac482590410c64315f
Kshitij Bhardwaj <kshitij.ghss@gmail.com>
2024-11-28 14:07:35 +0530

    fix: dependencies issue

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,31 +1,16 @@
-"use client"
+import { UserButton } from "@clerk/nextjs";
+import { getApiLimitCount } from "@/lib/api-limit";
+import MobileSidebar from "@/components/mobile-sidebar";
+import { checkSubscription } from "@/lib/subscription";
 
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { cn } from "@/lib/utils";
-
-interface navbarProps {}
-
-const Navbar: React.FC<navbarProps> = () => {
-  const router = useRouter();
-  const pathname = usePathname();
+const Navbar = async () => {
+  const apiLimitCount = await getApiLimitCount();
+  const isPro = await checkSubscription();
   return (
-    <div className="flex flex-row gap-2 justify-between p-2">
-      <p className="font-bold text-xl">GOD</p>
-      <div className="flex flex-row gap-2">
-        <button
-          className={cn("flex p-2 items-center justify-center border-black border rounded-3xl w-[6rem]", pathname === "/auth" ? "flex" : "hidden")}
-          type="button"
-          onClick={() => router.push("/auth")}
-        >
-          sign-in
-        </button>
-        <button
-          className="flex p-2 items-center justify-center border-black border rounded-3xl w-[6rem]"
-          type="button"
-          onClick={() => router.push("/auth")}
-        >
-          sign-up
-        </button>
+    <div className="flex items-center p-4">
+      <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
       </div>
     </div>
   );
